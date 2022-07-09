c332af5242f37fa3901d44a5583163313af8e70c
島田陸弥 <rikuya.61@i.softbank.jp>
2022-07-09 22:28:33 +0900

    top_page

---
+++ resources/lang/ja/label.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+  // ログイン・登録
+  'Register' => '登録',
+  'Name' => '名前',
+  'Login' => 'ログイン',
+  'Email' => 'メールアドレス',
+  'Password' => 'パスワード',
+  'Confirm Password' => '確認用パスワード',
+  'Remember Me' => '自動ログインにする',
+  'Forgot Your Password?' => 'パスワードをお忘れの方',
+];
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -12,7 +12,7 @@
 
             <div class="col-4"> 
                 <a href="#" class="thumbnail">
-                    <img src="{{ asset('resources/img/image1.jpg') }}" class="img-rounded" width="200" height="200"/>
+                    <img src="{{ asset('img/image1.jpg') }}" class="img-rounded" width="200" height="200"/>
                     <div class="card__txt">
                         <p class="card__title">title</p>
                             <p class="card__sub">place</p>
