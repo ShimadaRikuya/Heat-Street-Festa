16f34384e21f1409277170895a21cd4aa2d4cb9f
島田陸弥 <rikuya.61@i.softbank.jp>
2022-10-29 16:39:58 +0900

    feature/search

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -27,6 +27,47 @@ class EventController extends Controller
         return view('events.index', compact('events'));
     }
 
+    /**
+     * Display a listing of the search.
+     * 
+     * 検索機能
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function keyword(Request $request)
+    {
+        // 検索フォームで入力された値を取得する
+        $search = $request->input('search');
+        // クエリビルダ
+        $query = Event::query();
+        // もし検索フォームにキーワードが入力されたら
+        if ($search) {
+            // 全角スペースを半角に変換
+            $spaceConversion = mb_convert_kana($search, 's');
+
+            // 単語を半角スペースで区切り、配列にする（例："山田 翔" → ["山田", "翔"]）
+            $wordArraySearched = preg_split('/[\s,]+/', $spaceConversion, -1, PREG_SPLIT_NO_EMPTY);
+
+
+            // 単語をループで回し、ユーザーネームと部分一致するものがあれば、$queryとして保持される
+            foreach($wordArraySearched as $value) {
+                $query->where('title', 'like', '%'.$value.'%');
+            }
+            $events = $query->paginate(24);
+            return view('events.keyword', compact('events', 'search'));
+        }
+        return redirect('')->with('flash_message', 'キーワードを取得できませんでした');
+    }
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     * 
+     * カテゴリー別イベント
+     * 
+     */
     public function search(Event $event, $category_id, $category)
     {
         $events = Event::where('category_id', $category_id)->paginate(24);
